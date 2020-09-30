15892802c8e083bbb23045a10d58e13691eb8142
Ruben Tomas <irubentomas@gmail.com>
2020-09-30 02:22:44 +0100

    Videos project folder ready

---
+++ videos_app/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Accordion from "./Accordion/Accordion";
+import Header from "./Header";
+import Route from "./Routes/Route";
+import Translate from "./Tradutor/Translate";
+import SearchBar from "./Search/SearchbBar";
+import DropDown from "./DropDown/DropDown";
+
+const App = () => {
+
+  return(
+    <div>Hello</div>
+  );
+};
+
+export default App;
